d3179d34d20d8dca04ebd4e0f65b5032b3fb7010
Luka <lukhanyo95@gmail.com>
2022-10-09 18:17:38 +0200

    add javascript to index.html

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-const burger = document.getElementById('burger_menu');
-const navUl = document.querySelector('.nav_list');
+const burger = document.getElementById('burger__menu');
+const navUl = document.querySelector('.nav__list');
 
 burger.addEventListener('click', () =>{
     navUl.classList.toggle('show');
     burger.classList.toggle('change');
 })
+
+const typed = new Typed (".typing", {
+strings: ['Lukhanyo Matshebelele'],
+typeSpeed: 100,
+backSpeed: 60,
+loop: true
+});
